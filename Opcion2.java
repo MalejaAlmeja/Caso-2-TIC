f75f9bae6126cca04e86b68f37bc6e3588f3a68d
JuanJose Cortes <jj.cortesv1@uniandes.edu.co>
2025-09-14 12:31:05 -0500

    avances opcion 2

--- Opcion2.java
+++ Opcion2.java
@@ -1,11 +1,28 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Opcion2 {
     public static void ejecutarOpcion2(int NPROC, int NMARCOS) {
 
-
+        HashMap<Integer,Proceso> procesos = new HashMap<Integer, Proceso>();
+        int marcosProceso = NMARCOS/NPROC;
+        Queue<Integer> turnos = new LinkedList<>();
+        long[] contadoresLRU = new long[NMARCOS];
+        int[] marcosAsignados = new int[NMARCOS];
+        int[] dvEnProceso = new int[NPROC];
+        int[] referenciasProcesadas = new int[NPROC];
+        int[] marcosCargados = new int[NMARCOS];
+        Arrays.fill(marcosCargados,-1); //-1 significa que no hay nada cargado ahí
+        int TPglobal = 0; 
+        
+        System.out.println("Inicio:");
         for (int i = 0; i < NPROC ; i++)
         {
             System.out.println("PROC "+i+" == Leyendo archivo de configuración ==");
@@ -14,6 +31,7 @@ public class Opcion2 {
             int NC = 0;
             int NR = 0;
             int NP = 0;
+            ArrayList<Integer> listaDV = new ArrayList<Integer>();
             String archivoConfig = "proc"+i+".txt";
             try (BufferedReader br = new BufferedReader(new FileReader(archivoConfig))) {
             String linea;
@@ -34,13 +52,96 @@ public class Opcion2 {
                     NP = Integer.parseInt(linea.split("=")[1].trim());
                     System.out.println("PROC "+i+"leyendo NP. Num Paginas: "+NP);
                 }
+                else{
+                    List<String> referencia = Arrays.asList(linea.split(","));
+                    int dv = Integer.parseInt(referencia.get(1).trim())*TP+Integer.parseInt(referencia.get(2).trim());
+                    listaDV.add(dv);
+                }
             }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            for (int j=i*marcosProceso;j<i*marcosProceso+marcosProceso;j++)
+            {
+                
+                marcosAsignados[j] = i;
+                System.out.println("Proceso "+i+": recibe marco "+j);
+            }
+            TPglobal = TP;
             System.out.println("PROC "+i+"== Terminó de leer archivo de configuración ==");
+            Proceso proc_i = new Proceso(NMARCOS, NF, NC, NR, NP, listaDV);
+            dvEnProceso[i]=listaDV.get(0);
+            referenciasProcesadas[i] = dvEnProceso[i]/4; //4 por que son enteros
+            System.out.println(dvEnProceso[i]);
+            procesos.put(i, proc_i);
+            turnos.add(i);
+
         }
         
+
+        System.out.println("\n Simulación:");
+        
+        while (!turnos.isEmpty())
+        {
+            
+            int i = turnos.poll();
+            System.out.println("Turno proc: "+i);
+            Proceso proc_i = procesos.get(i);
+            System.out.println("PROC "+i+" analizando linea_: "+referenciasProcesadas[i]);
+            int paginaActual = (TPglobal*marcosProceso*i+dvEnProceso[i])/TPglobal;
+            //Análisis
+            int[] tablaPaginas = proc_i.getTP();
+            boolean falloPagina = false;
+            boolean hit = false;
+            if (tablaPaginas[paginaActual]==-1)
+            {
+                falloPagina = true;
+            }
+            else
+            {
+                hit = true;
+            }
+            if (falloPagina)
+            {
+                
+            }
+            if (hit)
+            {
+                System.out.println("PROC "+i+" hits: ");
+            }
+
+            //LRU
+            System.out.println("PROC "+i+" Envejecimiento"); // como se corren los bits de los anteriores 'ticks' se dice que envejece
+            for (int j= 0;j<NMARCOS;j++)
+            {
+                
+                contadoresLRU[j] >>= 1;
+                if (j==((TPglobal*marcosProceso*i+dvEnProceso[i])/TPglobal))
+                {
+
+                    contadoresLRU[j] |= (1L<<63); //Esto lo que hace es básicamente registrar el acceso en este 'tick de reloj'
+                }
+                
+            }
+            if ((dvEnProceso[i]+1)<proc_i.getListaDV().size())
+            {
+                turnos.add(i);
+                dvEnProceso[i] ++;
+
+            }
+            referenciasProcesadas[i]= dvEnProceso[i];
+           
+
+            
+
+
+
+
+
+
+
+        }
+
         System.out.println("Opción 2 ejecutada.");
     }
     
--- Proceso.java
+++ Proceso.java
@@ -1,10 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Proceso {
     
     private int[] TP;
+    private int NF;
+    private int NC;
+    private int NR;
+    private int NP;
+    private ArrayList<Integer> listaDV;
 
-    public Proceso(int NMARCOS)
+    public Proceso(int NMARCOS, int NF, int NC, int NR, int NP, ArrayList<Integer> listaDV)
     {
         this.TP = new int[NMARCOS];
+        Arrays.fill(this.TP,-1); //-1 significa que no tiene ningúna marco de página asignado
+        this.NF = NF;
+        this.NC = NC;
+        this.NR = NR;
+        this.NP = NP;
+        this.listaDV = listaDV;
     }
 
     public int[] getTP()
@@ -12,7 +26,28 @@ public class Proceso {
         return this.TP;
     }
 
-    public void modifyTP(int[] newTP)
+    public int getNF()
+    {
+        return this.NF;
+    }
+    public int getNC()
+    {
+        return this.NC;
+    }
+    public int getNR()
+    {
+        return this.NR;
+    }
+    public int getNP()
+    {
+        return this.NP;
+    }
+    public ArrayList<Integer> getListaDV()
+    {
+        return this.listaDV;
+    }
+
+    public void updateTP(int[] newTP)
     {
         this.TP = newTP;
     }
