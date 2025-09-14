a1713595fd5863348ed4d8c1f0d159faece34acd
Ma Alejandra Carrillo <ma.carrillo2@uniandes.edu.co>
2025-09-14 07:43:07 -0500

    Inicio Opcion2

--- Opcion2.java
+++ Opcion2.java
@@ -1,6 +1,12 @@
+
+
 public class Opcion2 {
     public static void ejecutarOpcion2(int NPROC, int NMARCOS) {
-
+        //Cargar archivos
+        for (int i = 0; i < NPROC; i++) {
+            String nombreArchivo = "proceso" + (i) + ".txt";
+            Proceso proceso = new Proceso();
+        }
 
 
         
